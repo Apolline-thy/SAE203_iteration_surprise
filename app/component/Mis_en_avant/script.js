f7cde05a890c37aacd26bb101a2af3eabb31615a
Thierry Apolline <thierry18@mmi-dev.unilim.fr>
2025-04-16 23:35:40 +0200

    tentative de  carousel

--- app/component/Mis_en_avant/script.js
+++ app/component/Mis_en_avant/script.js
@@ -7,20 +7,27 @@ let templateCards = await templateFile2.text();
 let Mis_en_avant = {};
 
 Mis_en_avant.format = function (obj) {
-  let html = template;
-  let cardsHTML = "";
+  let html = template; // Le template principal du carrousel
+  let cardsHTML = ""; // Cette variable va contenir toutes les cartes générées
 
+  // Pour chaque film, on génère une carte
   for (let c of obj) {
-    let card = templateCards;
+    let card = templateCards; // Template pour chaque carte
+
+    // Remplacer les placeholders dans le template de la carte
     card = card.replace("{{name}}", c.name);
     card = card.replace("{{image}}", c.image);
     card = card.replace("{{description}}", c.description);
     card = card.replace("{{handler}}", `C.handlerDetail(${c.id})`);
+    card = card.replaceAll("{{id}}", c.id);
 
+    // Ajouter chaque carte au HTML global des cartes
     cardsHTML += card;
   }
 
+  // Remplacer {{cards}} par toutes les cartes générées
   html = html.replace("{{cards}}", cardsHTML);
+
   return html;
 };
 
